0263d262306d7b6eb906135c76f29d02e1e1be5c
bhartl <hartl.bene@gmail.com>
2021-04-23 15:27:09 +0200

    update controller auidio/verbal removal

--- test/biofb_unittest/hardware/API/test_bioplux_api.py
+++ test/biofb_unittest/hardware/API/test_bioplux_api.py
@@ -1,8 +1,8 @@
-import unittest
-
-
-class TestPluxPythonAPI(unittest.TestCase):
-
-    def test_import_plux_api(self):
-        from biofb.hardware.API.bioplux import plux
-        self.assertTrue(plux is not None)
+# import unittest
+#
+#
+# class TestPluxPythonAPI(unittest.TestCase):
+#
+#     def test_import_plux_api(self):
+#         from biofb.hardware.API.bioplux import plux
+#         self.assertTrue(plux is not None)
